1356fc7b64e9fad370be6126dc195cda456b02d9
Michael Yosowitz <michaelyosowitz@gmail.com>
2018-04-22 09:08:41 -0700

    component-library 2

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-const Album = () => (
-    <section className="album">
-        Album Page here
-    </section>
-);
+class Album extends Component {
+    render () {
+        return (
+            <section className="album">
+                {this.props.match.params.slug} album here
+            </section>
+        );
+    }
+}
 
 export default Album;
