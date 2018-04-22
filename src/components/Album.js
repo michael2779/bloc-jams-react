167f282930f94acf7737f12f2ee467bf0c3f2e50
Michael Yosowitz <michaelyosowitz@gmail.com>
2018-04-22 11:29:53 -0700

    components-album

--- src/components/Album.js
+++ src/components/Album.js
@@ -1,10 +1,49 @@
 import React, { Component } from 'react';
+import albumData from '../data/albums';
 
 class Album extends Component {
+    constructor (props) {
+        super(props)
+        
+        const album = albumData.find ( album => {
+            return album.slug === this.props.match.params.slug
+        });
+
+        this.state = {
+            album: album
+        };
+    }
+
     render () {
         return (
             <section className="album">
-                {this.props.match.params.slug} album here
+                <section id="album-info">
+                    <img id="album-cover-art" src={this.state.album.albumCover} alt=""/>
+                    <div className="album-details">
+                        <h1 id="album-title">{this.state.album.title}</h1>
+                        <h2 className="artist">{this.state.album.artist}</h2>
+                        <div id="release-info">{this.state.album.releaseInfo}</div>
+                    </div>
+                </section>
+                <table id="song-list">
+                    <colgroup>
+                        <col id="song-number-column" />
+                        <col id="song-title-column" />
+                        <col id="song-duration-column" />
+                    </colgroup>  
+                    {
+                        this.state.album.songs.map( (song, index)  =>
+                            <tbody key = {index} >                         
+                                    {console.log(song)}
+                                    <tr>
+                                        <td>{index+1}</td>
+                                        <td>{song.title}</td>
+                                        <td>{song.duration}</td>
+                                    </tr>        
+                            </tbody>
+                        )
+                    }
+                </table>
             </section>
         );
     }
