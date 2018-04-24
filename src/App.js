d3bd40e1a8cb0348f94d0e32eccac242e652442f
Michael Yosowitz <michaelyosowitz@gmail.com>
2018-04-24 14:22:07 -0700

    recloned from git repo

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
+import Landing from './components/Landing';
+import Library from './components/Library';
+import Album from './components/Album';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header>
+          <h1>Bloc James</h1>
+        </header>
+        <nav>
+          <Link to='/'>Landing</Link>
+          <Link to='/library'>Library</Link>
+        </nav>
+        <main>
+          <Route exact path="/" component={Landing} />
+          <Route path="/library" component={Library} />
+          <Route path="/album/:slug" component={Album} />
+        </main>
+      </div>
+    );
+  }
+}
+
+export default App;
